acdb82f2f75dc1f9fa2995c552f7d81c71d786be
Edwin Biemond <edwin@Edwins-MacBook-Pro.local>
2014-08-25 22:14:57 -0500

    update puppet code

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -15,33 +15,14 @@ node 'admin.example.com' {
   include opatch
   include domains
   include nodemanager, startwls, userconfig
-  include users
-  include groups
-  include machines
-  include managed_servers
-  include managed_servers_channels
+  include security
+  include basic_config
   include datasources
-  include clusters
-  include coherence_clusters
-  include server_templates
   include virtual_hosts
-  include workmanager_constraints
   include workmanagers
   include file_persistence
-  include jms_servers
-  include jms_saf_agents
-  include jms_modules
-  include jms_module_subdeployments
-  include jms_module_quotas
-  include jms_module_cfs
-  include jms_module_queues_objects
-  include jms_module_topics_objects
-  include foreign_server_objects
-  include foreign_server_entries_objects
-  include saf_remote_context_objects
-  include saf_error_handlers
-  include saf_imported_destination
-  include saf_imported_destination_objects
+  include jms
+  include saf
   include pack_domain
 
   Class[java] -> Class[orawls::weblogic]
@@ -185,9 +166,6 @@ class java {
 
   include jdk7
 
-  # $javas = ["/usr/java/jdk1.7.0_51/jre/bin/java", "/usr/java/jdk1.7.0_51/bin/java"]
-  # $LOG_DIR='/tmp/log_puppet_weblogic'
-
   jdk7::install7{ 'jdk1.7.0_51':
       version                   => "7u51" ,
       fullVersion               => "jdk1.7.0_51",
@@ -199,32 +177,7 @@ class java {
       cryptographyExtensionFile => "UnlimitedJCEPolicyJDK7.zip",
       sourcePath                => "/software",
   }
-  # ->
-  # file { $LOG_DIR:
-  #   ensure  => directory,
-  #   mode    => '0777',
-  # }
-  # ->
-  # file { "$LOG_DIR/log.txt":
-  #   ensure  => file,
-  #   mode    => '0666'
-  # }
-  # ->
-  # javaexec_debug {$javas: }
-  # ->
-  # exec { 'java_debug start provisioning':
-  #   command => "${javas[0]} -version '+++ start provisioning +++'"
-  # }
 }
-
-# log all java executions:
-define javaexec_debug() {
-  exec { "patch java to log all executions on $title":
-    command => "/bin/mv ${title} ${title}_ && /bin/cp /vagrant/puppet/files/java_debug ${title} && /bin/chmod +x ${title}",
-    unless  => "/usr/bin/test -f ${title}_",
-  }
-}
-
 
 class bsu{
   require orawls::weblogic
@@ -293,216 +246,144 @@ class userconfig{
   create_resources('orawls::storeuserconfig',$userconfig_instances, $default_params)
 }
 
-class users{
+
+class security{
   require userconfig
   $default_params = {}
   $user_instances = hiera('user_instances', {})
   create_resources('wls_user',$user_instances, $default_params)
-}
 
-class groups{
-  require users
-  $default_params = {}
   $group_instances = hiera('group_instances', {})
   create_resources('wls_group',$group_instances, $default_params)
-}
 
-class authentication_providers{
-  require groups
-  $default_params = {}
   $authentication_provider_instances = hiera('authentication_provider_instances', {})
   create_resources('wls_authentication_provider',$authentication_provider_instances, $default_params)
 }
 
-class machines{
-  require authentication_providers
+class basic_config{
+  require security
   $default_params = {}
+
+  $wls_domain_instances = hiera('wls_domain_instances', {})
+  create_resources('wls_domain',$wls_domain_instances, $default_params)
+
+  # subscribe on changes
+  $wls_adminserver_instances = hiera('wls_adminserver_instances', {})
+  create_resources('wls_adminserver',$wls_adminserver_instances, $default_params)
+
   $machines_instances = hiera('machines_instances', {})
   create_resources('wls_machine',$machines_instances, $default_params)
-}
 
-class managed_servers{
-  require machines
-  $default_params = {}
   $server_instances = hiera('server_instances', {})
   create_resources('wls_server',$server_instances, $default_params)
-}
 
-class managed_servers_channels{
-  require managed_servers
-  $default_params = {}
   $server_channel_instances = hiera('server_channel_instances', {})
   create_resources('wls_server_channel',$server_channel_instances, $default_params)
+
+  $cluster_instances = hiera('cluster_instances', {})
+  create_resources('wls_cluster',$cluster_instances, $default_params)
+
+  $coherence_cluster_instances = hiera('coherence_cluster_instances', {})
+  create_resources('wls_coherence_cluster',$coherence_cluster_instances, $default_params)
+
+  $server_template_instances = hiera('server_template_instances', {})
+  create_resources('wls_server_template',$server_template_instances, $default_params)
+
 }
 
 class datasources{
-  require managed_servers_channels
+  require basic_config
   $default_params = {}
   $datasource_instances = hiera('datasource_instances', {})
   create_resources('wls_datasource',$datasource_instances, $default_params)
 }
 
-class clusters{
-  require datasources
-  $default_params = {}
-  $cluster_instances = hiera('cluster_instances', {})
-  create_resources('wls_cluster',$cluster_instances, $default_params)
-}
-
-class coherence_clusters{
-  require clusters
-  $default_params = {}
-  $coherence_cluster_instances = hiera('coherence_cluster_instances', {})
-  create_resources('wls_coherence_cluster',$coherence_cluster_instances, $default_params)
-}
-
-class server_templates{
-  require coherence_clusters
-  $default_params = {}
-  $server_template_instances = hiera('server_template_instances', {})
-  create_resources('wls_server_template',$server_template_instances, $default_params)
-}
 
 class virtual_hosts{
-  require server_templates
+  require datasources
   $default_params = {}
   $virtual_host_instances = hiera('virtual_host_instances', {})
   create_resources('wls_virtual_host',$virtual_host_instances, $default_params)
 }
 
-class workmanager_constraints{
+class workmanagers{
   require virtual_hosts
   $default_params = {}
+
   $workmanager_constraint_instances = hiera('workmanager_constraint_instances', {})
   create_resources('wls_workmanager_constraint',$workmanager_constraint_instances, $default_params)
-}
 
-class workmanagers{
-  require workmanager_constraints
-  $default_params = {}
   $workmanager_instances = hiera('workmanager_instances', {})
   create_resources('wls_workmanager',$workmanager_instances, $default_params)
 }
 
-class file_persistence_folders{
+class file_persistence{
   require workmanagers
 
   $default_params = {}
+
   $file_persistence_folders = hiera('file_persistence_folders', {})
   create_resources('file',$file_persistence_folders, $default_params)
-}
-
-class file_persistence{
-  require file_persistence_folders
-
-  $default_params = {}
 
   $file_persistence_store_instances = hiera('file_persistence_store_instances', {})
   create_resources('wls_file_persistence_store',$file_persistence_store_instances, $default_params)
 }
 
-class jms_servers{
+class jms{
   require file_persistence
+
   $default_params = {}
   $jmsserver_instances = hiera('jmsserver_instances', {})
   create_resources('wls_jmsserver',$jmsserver_instances, $default_params)
-}
 
-class jms_saf_agents{
-  require jms_servers
-  $default_params = {}
-  $safagent_instances = hiera('safagent_instances', {})
-  create_resources('wls_safagent',$safagent_instances, $default_params)
-}
-
-class jms_modules{
-  require jms_saf_agents
-  $default_params = {}
   $jms_module_instances = hiera('jms_module_instances', {})
   create_resources('wls_jms_module',$jms_module_instances, $default_params)
-}
 
-class jms_module_subdeployments{
-  require jms_modules
-  $default_params = {}
   $jms_subdeployment_instances = hiera('jms_subdeployment_instances', {})
   create_resources('wls_jms_subdeployment',$jms_subdeployment_instances, $default_params)
-}
 
-class jms_module_quotas{
-  require jms_module_subdeployments
-  $default_params = {}
   $jms_quota_instances = hiera('jms_quota_instances', {})
   create_resources('wls_jms_quota',$jms_quota_instances, $default_params)
-}
 
-class jms_module_cfs{
-  require jms_module_quotas
-  $default_params = {}
   $jms_connection_factory_instances = hiera('jms_connection_factory_instances', {})
   create_resources('wls_jms_connection_factory',$jms_connection_factory_instances, $default_params)
-}
 
-class jms_module_queues_objects{
-  require jms_module_cfs
-  $default_params = {}
   $jms_queue_instances = hiera('jms_queue_instances', {})
   create_resources('wls_jms_queue',$jms_queue_instances, $default_params)
-}
 
-class jms_module_topics_objects{
-  require jms_module_queues_objects
-  $default_params = {}
   $jms_topic_instances = hiera('jms_topic_instances', {})
   create_resources('wls_jms_topic',$jms_topic_instances, $default_params)
-}
 
-class foreign_server_objects{
-  require jms_module_topics_objects
-  $default_params = {}
   $foreign_server_instances = hiera('foreign_server_instances', {})
   create_resources('wls_foreign_server',$foreign_server_instances, $default_params)
-}
 
-class foreign_server_entries_objects{
-  require foreign_server_objects
-  $default_params = {}
   $foreign_server_object_instances = hiera('foreign_server_object_instances', {})
   create_resources('wls_foreign_server_object',$foreign_server_object_instances, $default_params)
 }
 
-class saf_remote_context_objects {
-  require foreign_server_entries_objects
+class saf{
+  require jms
+
   $default_params = {}
+  $safagent_instances = hiera('safagent_instances', {})
+  create_resources('wls_safagent',$safagent_instances, $default_params)
+
   $saf_remote_context_instances = hiera('saf_remote_context_instances', {})
   create_resources('wls_saf_remote_context',$saf_remote_context_instances, $default_params)
-}
 
-class saf_error_handlers {
-  require saf_remote_context_objects
-  $default_params = {}
   $saf_error_handler_instances = hiera('saf_error_handler_instances', {})
   create_resources('wls_saf_error_handler',$saf_error_handler_instances, $default_params)
-}
 
-class saf_imported_destination {
-  require saf_error_handlers
-  $default_params = {}
   $saf_imported_destination_instances = hiera('saf_imported_destination_instances', {})
   create_resources('wls_saf_imported_destination',$saf_imported_destination_instances, $default_params)
-}
 
-class saf_imported_destination_objects {
-  require saf_imported_destination
-  $default_params = {}
   $saf_imported_destination_object_instances = hiera('saf_imported_destination_object_instances', {})
   create_resources('wls_saf_imported_destination_object',$saf_imported_destination_object_instances, $default_params)
 }
 
-
-
 class pack_domain{
-  require saf_imported_destination_objects
+  require saf
+
   $default_params = {}
   $pack_domain_instances = hiera('pack_domain_instances', $default_params)
   create_resources('orawls::packdomain',$pack_domain_instances, $default_params)
